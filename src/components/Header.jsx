fb280cfd8fb84389a206c2207f5d8771fe24e945
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-05-31 18:33:43 -0500

    Finalizando la parte de maquetación e integrración de iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/components/Header.css';
 
@@ -7,16 +8,22 @@ const Header = () => {
     <div className="Header">
       {/* <h1 className="Header-title">PlatziConf Merch</h1> */}
       <div className="Header-title">
-        <img
-          src="https://static.platzi.com/commons/assets/image/isotipoPlatzi093f27a2fb00922bb105.png"
-          alt="Logo_Verde_platzi"
-        />
-        <img
-          src="https://static.platzi.com/commons/assets/image/logotipo-platzie69328f33899695e31fa.png"
-          alt="Logo_Platzi"
-        />
+        <Link to="/">
+          <img
+            src="https://static.platzi.com/commons/assets/image/isotipoPlatzi093f27a2fb00922bb105.png"
+            alt="Logo_Verde_platzi"
+          />
+          <img
+            src="https://static.platzi.com/commons/assets/image/logotipo-platzie69328f33899695e31fa.png"
+            alt="Logo_Platzi"
+          />
+        </Link>
       </div>
-      <div className="Header-checkout">Checkout</div>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket" style={{ color: '#eee' }} />
+        </Link>
+      </div>
     </div>
   );
 };
