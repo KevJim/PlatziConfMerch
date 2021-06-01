48d3029df007230ddbca8fe4b4d18af58c9c89af
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-05-31 21:39:10 -0500

    Empezando integración de paypal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Header.css';
 
 const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
   return (
     <div className="Header">
       {/* <h1 className="Header-title">PlatziConf Merch</h1> */}
@@ -23,6 +27,7 @@ const Header = () => {
         <Link to="/checkout">
           <i className="fas fa-shopping-basket" style={{ color: '#eee' }} />
         </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
       </div>
     </div>
   );
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { PayPalButton } from 'react-paypal-button-v2';
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Payment.css';
 
 const Payment = () => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+  const history = useHistory();
+
+  const paypalOptions = {
+    clientId: '',
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    color: 'blue',
+    shape: 'rect',
+    label: 'paypal',
+  };
+
+  const handlePaymentSuccess = (data) => {
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      history.push('/checkout/success');
+    }
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumalator, currentValue) =>
+      accumalator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido:</h3>
-        <div className="Payment-button">Boton de pago con Paypal</div>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PayPalButton
+            options={paypalOptions}
+            style={buttonStyles}
+            amount={handleSumTotal()}
+            onButtonReady={() => console.log('Start Payment')}
+            onSuccess={(data) => handlePaymentSuccess(data)}
+            onError={(error) => console.log(error)}
+          />
+        </div>
       </div>
     </div>
   );
